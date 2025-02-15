ce272e6a252113495ed2fcb471b4821ebd0eeead
frontendmakaidigitals <frontendmakaidigitals@gmail.com>
2025-02-15 13:33:12 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -188,7 +188,7 @@ const Page = () => {
                         ))}
                       </motion.article>
                       <p className="mt-3">
-                        At Insight Vision, we&apos;re here to help you elevate
+                        At Interzen, we&apos;re here to help you elevate
                         your brand and reach your business goals. Whether
                         you&apos;re looking for a marketing strategy or just
                         need expert advice, our team is ready to assist.
