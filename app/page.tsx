7bf91fa4f4300121cd17efc758b547120d857979
frontendmakaidigitals <frontendmakaidigitals@gmail.com>
2025-02-15 13:35:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import AboutUs from "./(Home Page)/AboutUs";
 import TechStack from "./(Home Page)/TechStack";
 import { CompantStats } from "./(Home Page)/CompantStats";
 import MaskSection from "./(Home Page)/MaskSection";
-import InstaVideos from "./(Home Page)/InstaVideos";
 import Slider from "./(Home Page)/Slider";
 import Form from "./App chunks/components/Form";
 import { AnimatePresence, motion } from "framer-motion";
@@ -33,7 +32,6 @@ export default function Home() {
         <Slider />
         <Form />
         <MarqueeLogo />
-        <InstaVideos />
       </motion.div>
     </AnimatePresence>
   );
